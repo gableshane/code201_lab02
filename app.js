d5df0c8a1851b30fed5d623271e1f2f152aafcd9
Shane Gable <gableshane@gmail.com>
2019-11-10 09:49:04 -0800

    rebuilt guessing game. Final version

--- app.js
+++ app.js
@@ -1,130 +1,93 @@
 'use strict';
+// User starting score
 var score = 0;
-// Asks the user for their name and returns a greeting.
-var user_name = prompt('Welcome, what is your name?');
-alert('Welcome ' + user_name + '. This is Shane\'s guessing game!');
-
-function yesCorrect (question) {
-    if (question.toUpperCase() === 'YES' || question.toUpperCase() === 'Y') {
-        // console.log('The user has guessed question 3 correctly.');
-         alert('Good job. You guessed correctly');
-         score++;
-     } else if (question.toUpperCase() === 'NO' || question.toUpperCase() === 'N') {
-        // console.log('The user has guessed question 3 incorrectly.');
-         alert('Oops. You guessed incorrectly.');
-     } else {
-        // console.log('The user has provided an invalid answer to question3');
-         alert('Sorry, that is not a valid answer');
-     }
+// List of questions for guessing game. Array's in [answer,question] format.
+var questionOne = ['NO', 'Was Shane ever in the military?'];
+var questionTwo = ['NO', 'Does Shane own a cat?'];
+var questionThree = ['YES', 'Is Shane\'s birthday ever on Thanksgiving?'];
+var questionFour = ['YES', 'Does Shane own more than one pair of shoes?'];
+var questionFive = ['YES', 'Is Shane allergic to dairy?'];
+var questionSix = [42, 'What is Shane\s favorite number?'];
+var questionSeven = [['chapstick', 'airpods', 'cash', 'debit card', 'license'], 'What do you think Shane has in his pockets?']
+// Asks user their name and welcomes them
+function welcomeToGame() {
+    var userName = prompt('What is your name?');
+    alert('Welcome to my guessing game ' + userName);
 }
-
-function noCorrect (question) {
-    if (question.toUpperCase() === 'YES' || question.toUpperCase() === 'Y') {
-        // console.log('The user guessed question 1 incorrectly.');
-         alert('Oops. You guessed incorrectly.');
-     } else if (question.toUpperCase() === 'NO' || question.toUpperCase() === 'N') {
-        // console.log('The user has guessed question 1 correctly.');
-         alert('Good job! You guessed correctly.');
-         score++;
-     } else {
-        // console.log('The user has provided an invalid answer to question 1');
-         alert('Sorry, that is not a valid answer.');
-     }
-}
-
-// GUESS NUM FUNCTION
-function is42 (num) {
-    if (numberGuess > 42) {
-        alert('You guessed too high!');
-        counter++;
-    } else if (numberGuess < 42) {
-        alert('You guessed too low!');
-        counter++;
-    } else if (numberGuess === 42) {
-        alert('That is correct');
-        score++;
-    }
-        else {
-        alert('That is not a number');
+// Yes/No question function
+function yesNoQuestion (questionArr) {
+    var flag;
+    while(!flag) {
+        var answer = prompt(questionArr[1]);
+        if (answer.toUpperCase() === 'YES' || answer.toUpperCase() === 'NO') {
+            if (questionArr[0] === answer.toUpperCase()) {
+                alert('Correct!');
+                score++;
+                flag = true;
+            } else {
+                alert('Incorrect.');
+                flag = true;
+            }
+        } else {
+            alert('Yes/No answers only please');
+        }
     }
 }
-// END OF GUESS FUNCTION
-
-// GUESS ITEM IN ARRAY FUNCTION
-function isIn (item, arr) {
-    for (var i = 0; i < arr.length; i++) {
-        if (item === arr[i]) {
-            alert('Correct! Shane does have a ' + item + ' in his pocket');
+// Number guessing function
+function guessNum (questionArr) {
+    var counter = 1;
+    if (counter === 5) {
+        alert('Out of guesses!');
+    }
+    while (counter < 5) {
+        var guess = parseInt(prompt(questionArr[1]));
+        if (guess === questionArr[0]) {
+            alert('Correct!');
             score++;
             break;
-        }
-    
-        else if (!flag) {
-            item = prompt('Incorrect. Guess again!');
+        } else if (guess > questionArr[0]) {
+            alert('Too high! Guess again');
             counter++;
-            if (counter > 5) {
+        } else if (guess < questionArr[0]) {
+            alert('Too low! Guess again');
+            counter++;
+        } else {
+            alert('Please guess a number.');
+        }
+    }
+}
+// Guess what is in this array function
+function arrayGuess (questionArr) {
+    var answers = questionArr[0];
+    var counter = 1;
+    while (counter < 8) {
+        if (counter !== 1) {
+            alert('Guess again!')
+        }
+        counter++;
+        if (counter === 8) {
+            alert('You have run out of guesses');
+            break;
+        }
+        var guess = prompt(questionArr[1]);
+        for (var i = 0; i < answers.length; i++) {
+            if (guess === answers[i]) {
+                alert('Correct!');
+                score++;
+                counter = 9;
                 break;
             }
         }
-    } 
-} 
-// END OF GUESS ITEM IN ARRAY FUNCTION
-
-// Asks the user the first question, evaluates, logs, and returns an alert.
-var question_1 = prompt('Was Shane ever in the military?');
-noCorrect(question_1);
-
-// Asks the user the second question, evaluates, logs, and returns an alert.
-var question_2 = prompt('Does Shane own a cat?');
-noCorrect(question_2);
-
-// Asks the user the third question, evaluates, logs, and returns an alert.
-var question_3 = prompt('Is Shane\'s birthday ever on Thanksgiving?');
-yesCorrect(question_3);
-
-// Asks the user the fourth question, evaluates, logs, and returns an alert.
-var question_4 = prompt('Does Shane own more than one pair of shoes?');
-yesCorrect(question_4);
-
-// Ask the user the fifth and final question, evaluates, logs, and returns an alert.
-var question_5 = prompt('Is Shane allergic to dairy?');
-yesCorrect(question_5);
-
-var numberGuess;
-var counter = 1;
-// User is prompted to guess a number
-while (numberGuess !== 42) {
-    numberGuess = parseInt(prompt('What is Shane\'s favorite number?'));
-
-    if (counter >= 4) { // breaks loop if user runs out of guesses
-        alert('You have run out of guesses. The correct answer is 42. Shane thinks that makes him seem intellectual, but it doesn\'t.');
-        console.log('The user ran out of guesses. ');
-        break;
-    } // Tells the user if they guessed too high/low or pushes back if not a number.
-    is42(numberGuess);
+    }
 }
-console.log('The user guessed correctly on their ' + counter + ' guess.');
 
-var things = ['chapstick', 'airpods', 'cash', 'debit card', 'license'];
-var guess = prompt('What do you think Shane has in his pockets?');
-var flag;
-counter = 1;
-
-isIn(guess, things);
-// for (var i = 0; i < things.length; i++) {
-//     if (guess === things[i]) {
-//         alert('Correct! Shane does have a ' + guess + ' in his pocket');
-//         score++;
-//         break;
-//     }
-
-//     else if (!flag) {
-//         guess = prompt('Incorrect. Guess again!');
-//         counter++;
-//         if (counter > 5) {
-//             break;
-//         }
-//     }
-// } 
-
-alert('Your score is ' + score); 
+// All the function calls that run the guessing game.
+welcomeToGame();
+yesNoQuestion(questionOne);
+yesNoQuestion(questionTwo);
+yesNoQuestion(questionThree);
+yesNoQuestion(questionFour);
+yesNoQuestion(questionFive);
+guessNum(questionSix);
+arrayGuess(questionSeven);
+alert('Your score is ' + score);
